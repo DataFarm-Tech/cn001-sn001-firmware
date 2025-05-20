b23147cf2d4e22608ee67cb75d58c944cab3d8fa
Lucas Aponso <s3896348@student.rmit.edu.au>
2025-05-20 14:08:48 +1000

    fix: fixed bug

--- src/bms.cpp
+++ src/bms.cpp
@@ -2,18 +2,8 @@
 #include "hw.h"
 #include "Arduino.h"
 
-typedef enum
-{
-    BAT_CHARGE_UNDEFINED = 0,
-    BAT_CHARGE_FULL = 1,        // > 75%
-    BAT_CHARGE_HIGH = 2,        // 50%–75%
-    BAT_CHARGE_MEDIUM = 3,      // 25%–50%
-    BAT_CHARGE_LOW = 4,         // 3%–25%
-    BAT_CHARGE_CRITICAL = 5     // < 3%
-} bat_charge_state_t;
 
-
-bat_charge_state_t battery_state = BAT_CHARGE_UNDEFINED;
+volatile bat_charge_state_t battery_state = BAT_CHARGE_UNDEFINED;
 
 /**
  * @brief Retrieves all PIN states to calculate the 
